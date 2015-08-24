3d45b04ab66bbfd940d1c5bbc0d9e461453a24de
danpeter <danpeter@kth.se>
2015-08-24 20:22:31 +0200

    adding some nice shading

--- src/main/webapp/js/card.js
+++ src/main/webapp/js/card.js
@@ -14,6 +14,10 @@ Card.h = 181;
 Card.prototype.draw = function (ctx) {
     var card = this;
     var imageName = (this.value + '_of_' + this.suit).toLowerCase();
+    ctx.shadowColor = '#191919';
+    ctx.shadowBlur = 20;
+    ctx.shadowOffsetX = 3;
+    ctx.shadowOffsetY = 3;
     ctx.drawImage(Game.canvasState.images[imageName], card.x, card.y, Card.w, Card.h);
 };
 
